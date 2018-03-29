96f7f5459ec217f2ec116b0e1163a9862ab3a2bd
fabianobrancher <fabiano.brancher@hotmail.com>
2018-03-29 19:43:41 -0300

    Editando categorias

--- src/Produtos.js
+++ src/Produtos.js
@@ -4,68 +4,92 @@ import axios from 'axios';
 import ProdutosHome from './ProdutosHome';
 import Categoria from './Categoria';
 
+// import Api from './Api';
+
 class Produtos extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            categorias: []
+            editCategoria: ''
         }
+ 
         this.handleNewCategoria = this.handleNewCategoria.bind(this);
-        this.loadCategorias = this.loadCategorias.bind(this);
         this.renderCategoria = this.renderCategoria.bind(this);
-    };
-    loadCategorias() {
-        // buscar as categorias
-        axios
-            .get('http://localhost:3001/categorias')
-            .then(res => {
-                    this.setState({
-                        categorias: res.data
-                    });
-                });
-    };
-    removeCategoria(categoria) {
-        console.log('Removendo categoria:', categoria.categoria);
-        axios
-            .delete('http://localhost:3001/categorias/'+categoria.id)
-            .then((res) => this.loadCategorias() );
-    };
+        this.editCategoria = this.editCategoria.bind(this);
+        this.cancelEditing = this.cancelEditing.bind(this);
+        this.handleEditCategoria = this.handleEditCategoria.bind(this);
+   };
+
     componentDidMount() {
-        this.loadCategorias();
+        this.props.loadCategorias();
     };
-    // <Link to="/produtos/categoria/1"> Categoria 1 </Link>
-    // {JSON.stringify(this.state.categorias)}
+
+    editCategoria(categoria) {
+        this.setState({
+            editCategoria: categoria.id
+        });
+    };
+
+    cancelEditing() {
+        this.setState({
+            editCategoria: ''
+        });
+    };
+
+    handleEditCategoria(key) {
+        if (key.keyCode === 13) {
+        //    console.log(this.refs['cat'+this.state.editCategoria].value);
+            this.props.editCategoria({
+                id: this.state.editCategoria,
+                categoria: this.refs['cat'+this.state.editCategoria].value
+            });
+            this.setState({
+                editCategoria: ''
+            });
+        };
+    };
+
     renderCategoria(cat) {
         return (
             <li key={cat.id}>
-                <button className=" btn btn-sm" onClick={() => this.removeCategoria(cat)}>
-                    <span className="glyphicon glyphicon-remove"> </span>
-                </button>
-                <Link to={`/produtos/categoria/${cat.id}`} >{cat.categoria}</Link>
-
+                { this.state.editCategoria === cat.id &&
+                    <div className="input-group">
+                        <div className="input-group-btn">
+                            <input ref={'cat'+cat.id} className="form-control" type="text" defaultValue={cat.categoria} onKeyUp={this.handleEditCategoria}/>
+                            <button className="btn" onClick={this.cancelEditing}>Cancel </button>
+                        </div>
+                    </div>
+                }
+                { this.state.editCategoria !== cat.id &&
+                    <div>
+                        <button className=" btn btn-sm" onClick={() => this.props.removeCategoria(cat)}>
+                            <span className="glyphicon glyphicon-remove"> </span>
+                        </button>
+                        <button className="btn btn-sm" onClick={() => this.editCategoria(cat)}>
+                            <span className="glyphicon glyphicon-pencil"> </span>
+                        </button>
+                        <Link to={`/produtos/categoria/${cat.id}`} >{cat.categoria}</Link>
+                    </div>
+                }
             </li>
         )
     };
     handleNewCategoria(key) {
         if (key.keyCode === 13) {
-            axios
-                .post('http://localhost:3001/categorias', {
-                    categoria: this.refs.categoria.value
-                })
-                .then(res => {
-                    this.loadCategorias();
-                    this.refs.categoria.value = '';
-                });
+            this.props.createCategoria({
+                categoria: this.refs.categoria.value
+            })
+            this.refs.categoria.value = '';
         }
     };
     render() {
-        const { match } = this.props;
-        const { categorias } = this.state;
+        const { match, categorias } = this.props;
         return (
             <div className="row">
                 <div className="col-md-2">
                     <h3> Categorias </h3>
-                    <ul>
+                    <ul style={{listStyle: 'none', padding: 0}}>
                         {categorias.map(this.renderCategoria)} 
                     </ul>
                     <div className="well well-sm">
